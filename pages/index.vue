8312770f9214c986a133605bfb9b16dbbf0dbfb9
Jordy Pereira <jordy.pereira@hotmail.com>
2018-12-06 06:43:35 +0100

    responsive reset button

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="container">
     <div>
-      <button
-        class="absolute pin-r m-5 px-5 py-2 bg-pink-lighter hover:bg-pink-lightest rounded-full"
-        @click="resetCats()"
-      >Reset</button>
       <h1 class="title">catvent</h1>
       <h2 class="subtitle">An advent calendar with cats.</h2>
+      <button
+        class="md:absolute md:pin-r m-5 px-5 py-2 bg-pink-lighter hover:bg-pink-lightest rounded-full"
+        @click="resetCats()"
+      >Reset</button>
       <div class="catvent-squares flex flex-wrap justify-around">
         <CatventSquare v-for="i in 25" :key="i" class="m-5" :id="i"/>
       </div>
